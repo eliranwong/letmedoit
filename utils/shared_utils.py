7a66cdc8014ef6e826438bfada6fba45495e5231
eliranwong <hkwebtech@gmail.com>
2023-10-14 16:31:35 +0100

    testing

--- utils/shared_utils.py
+++ utils/shared_utils.py
@@ -62,6 +62,7 @@ class SharedUtil:
             #command = f"""am start -n com.android.chrome/com.google.android.apps.chrome.Main -d {url}"""
             #pydoc.pipepager("", cmd=f"""termux-open-url {url}""")
             command = f'''am start -a android.intent.action.VIEW -n com.android.chrome/.Main -d {url}'''
-            SharedUtil.runSystemCommand(command)
+            #SharedUtil.runSystemCommand(command)
+            pydoc.pipepager("", cmd=command)
         else:
             SharedUtil.openURL(url)
