6a99f42da538f04b9b1f4745b1b5bdfe7c2fe77b
Eliran Wong <hkwebtech@gmail.com>
2023-10-25 16:18:04 +0100

    simplified setup files

--- setup.bat
+++ setup.bat
@@ -1,4 +1,2 @@
 python -m venv venv
-.\venv\Scripts\activate
-pip3 install -r requirements.txt
 python myHand.py
--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 python3 myHand.py
