f86430ec7b9131f879d5de0dab4e20acef463916
Eliran Wong <hkwebtech@gmail.com>
2023-11-14 09:53:59 +0000

    updated token limit

--- utils/shared_utils.py
+++ utils/shared_utils.py
@@ -13,8 +13,8 @@ class SharedUtil:
         #"gpt-3.5-turbo-instruct": 4097,
         "gpt-3.5-turbo": 4097,
         "gpt-3.5-turbo-16k": 16385,
-        "gpt-4-1106-preview": 128000,
-        "gpt-4-vision-preview": 128000,
+        "gpt-4-1106-preview": 8190,
+        "gpt-4-vision-preview": 8190,
         "gpt-4": 8192,
         "gpt-4-32k": 32768,
     }
