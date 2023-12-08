7585ddc5a66d6756a16d296d4b1cfe8e8149e5ea
Eliran Wong <hkwebtech@gmail.com>
2023-12-08 18:24:56 +0000

    v1.9.2 minor touch on plugin "contexts"

--- package/letmedoit/plugins/contexts.py
+++ package/letmedoit/plugins/contexts.py
@@ -6,12 +6,12 @@ LetMeDoIt AI Plugin - contexts
 add pre-defined contexts
 """
 
-config.predefinedContexts["Let me Summarize"] = """Provide me with a summary of the following content:
+config.predefinedContexts["Let me Summarize"] = """Provide me with a summary of the following content, which is delimited with XML tags "content":
 [NO_FUNCTION_CALL]"""
 
-config.predefinedContexts["Let me Explain"] = """Explain the following content to me:
+config.predefinedContexts["Let me Explain"] = """Explain the following content, which is delimited with XML tags "content", to me:
 [NO_FUNCTION_CALL]"""
 
 config.predefinedContexts["Let me Translate"] = """Assist me by acting as a translator.
-Please translate the content below:
+Please translate the content below, which is delimited with XML tags "content":
 [NO_FUNCTION_CALL]"""
