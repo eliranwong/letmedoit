40a6ec96893f108aade231ee0865ed6f04aa1f88
eliranwong <hkwebtech@gmail.com>
2024-03-11 14:05:42 +0000

    v2.1.73; fixed loading system tray

--- package/letmedoit/systemtray.py
+++ package/letmedoit/systemtray.py
@@ -29,7 +29,6 @@ config.divider = "--------------------"
 SharedUtil.setOsOpenCmd()
 
 import os, sys, platform, shutil
-from letmedoit.quicktask import QuickTask
 from letmedoit.gui.chatgui import ChatGui
 from PySide6.QtWidgets import QSystemTrayIcon, QMenu, QApplication
 from PySide6.QtGui import QIcon, QAction, QGuiApplication
